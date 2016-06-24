157e024099d8dd096640105f4c3541f4db46235b
Cordavi <michaelamundsen@gmail.com>
2016-06-23 20:43:28 -0400

    Completed method/Views so tests passes

---
+++ PrimeTime/FISPrimeTimeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISPrimeTimeTableViewController.h
+//  
+//
+//  Created by Michael Amundsen on 6/23/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISPrimeTimeTableViewController : UITableViewController
+
+@end
